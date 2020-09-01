49656bf93e1495ac7b0dc65936519f445e903a3c
Claudio Jiménez Flores <59317072+itsnectar@users.noreply.github.com>
2020-09-01 16:19:42 -0400

    Añadido firebase json

--- app/src/main/java/com/example/proyectotitulo/MainActivity.java
+++ app/src/main/java/com/example/proyectotitulo/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //WEAEWAEWAEAWEAEWAWEWA
-        //kjdheygs
+        //WEAEWAEWAEAWEAEWAWEWA2222
+        //kjdheygs22222
     }
 }
