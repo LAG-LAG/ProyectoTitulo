534e3a0f1863a5a7fc81bcc7e15e7d8fd4d2525b
Lucas Alarcon <lucas.alarcon.g@mail.pucv.cl>
2020-09-01 16:57:10 -0400

    login Añadido

--- app/src/main/java/com/example/proyectotitulo/Login.java
+++ app/src/main/java/com/example/proyectotitulo/Login.java
@@ -9,12 +9,8 @@ public class Login extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-<<<<<<< HEAD:app/src/main/java/com/example/proyectotitulo/MainActivity.java
-        setContentView(R.layout.activity_main);
-=======
-        setContentView(R.layout.login);
 
->>>>>>> Nect:app/src/main/java/com/example/proyectotitulo/Login.java
+        setContentView(R.layout.login);
 
     }
 }
