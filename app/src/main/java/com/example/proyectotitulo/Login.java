e0d1fc31bbdc5934bf780c7f29d068dd089ab6f9
Claudio Jiménez Flores <59317072+itsnectar@users.noreply.github.com>
2020-09-01 19:13:08 -0400

    Login con facebook funcional + muchos añadidos en el gradle

--- app/src/main/java/com/example/proyectotitulo/Login.java
+++ app/src/main/java/com/example/proyectotitulo/Login.java
@@ -1,25 +1,39 @@
 package com.example.proyectotitulo;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
 
 public class Login extends AppCompatActivity {
 
     private Button mIngresarBtn;
     private Button mRregistrarBtn;
+    private ImageView mFacebookBtn;
+    private CallbackManager callbackManager = CallbackManager.Factory.create();
     private EditText mEmail;
     private EditText mPassword;
 
@@ -30,9 +44,10 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
-
         mIngresarBtn = (Button) findViewById(R.id.ingresarBtn);
         mRregistrarBtn = (Button) findViewById(R.id.registrarBtn);
+        mFacebookBtn = (ImageView) findViewById(R.id.FacebookBtn);
+
         mPassword = (EditText) findViewById(R.id.passwordInput);
         mEmail = (EditText) findViewById(R.id.emailInput);
 
@@ -74,6 +89,51 @@ public class Login extends AppCompatActivity {
                 startActivity(intent);
                 finish();
                 return;
+            }
+        });
+
+        //Presionar imagen de fb
+        mFacebookBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LoginManager.getInstance().logInWithReadPermissions(Login.this, Arrays.asList("email","public_profile"));
+                LoginManager.getInstance().registerCallback(callbackManager,
+                        new FacebookCallback<LoginResult>() {
+                            @Override
+                            public void onSuccess(LoginResult loginResult) {
+                                handleFacebookAccessToken(loginResult.getAccessToken());
+                            }
+
+                            @Override
+                            public void onCancel() {
+                                // App code
+                            }
+
+                            @Override
+                            public void onError(FacebookException exception) {
+                                // App code
+                            }
+                        });            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode,resultCode,data);
+    }
+
+    private void handleFacebookAccessToken(AccessToken token) {
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        mAuth.signInWithCredential(credential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(Login.this, "Ingreso Correctamente.",Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText(Login.this, "No puede ingresar con esta cuenta.",Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
