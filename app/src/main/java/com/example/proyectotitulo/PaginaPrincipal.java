bf5bea8b682eb6763a89f5951f155b201f57c553
Lucas Alarcon <lucas.alarcon.g@mail.pucv.cl>
2020-09-02 16:13:18 -0400

    Toolbar vacia

--- app/src/main/java/com/example/proyectotitulo/PaginaPrincipal.java
+++ app/src/main/java/com/example/proyectotitulo/PaginaPrincipal.java
@@ -1,6 +1,7 @@
 package com.example.proyectotitulo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
@@ -10,5 +11,8 @@ public class PaginaPrincipal extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pagina_principal);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
