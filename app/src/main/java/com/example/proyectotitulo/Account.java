ce5670e238794a3c843df3a664174153a62956f7
Lucas Alarcon <lucas.alarcon.g@mail.pucv.cl>
2020-09-03 16:59:27 -0400

    Toolbar Menu Funcionando

--- app/src/main/java/com/example/proyectotitulo/Account.java
+++ app/src/main/java/com/example/proyectotitulo/Account.java
@@ -1,10 +1,14 @@
 package com.example.proyectotitulo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 public class Account extends AppCompatActivity {
 
@@ -19,5 +23,32 @@ public class Account extends AppCompatActivity {
         startActivity(intent);
         finish();
         return;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.accountBtn:
+                Toast.makeText(getApplicationContext(), "Account", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.chatBtn:
+                Toast.makeText(getApplicationContext(), "Chat", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.addBtn:
+                Toast.makeText(getApplicationContext(), "Add", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.likeBtn:
+                Toast.makeText(getApplicationContext(), "Like", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.publicacionesBtn:
+                Toast.makeText(getApplicationContext(), "Publicaciones", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
