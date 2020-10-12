0acdb2976294823a1322583075616385c7857fac
Lucas Alarcon <lucas.alarcon.g@mail.pucv.cl>
2020-10-12 17:18:27 -0300

    activity_valorar.xml con variables

--- app/src/main/java/com/example/proyectotitulo/Valorar.java
+++ app/src/main/java/com/example/proyectotitulo/Valorar.java
@@ -2,12 +2,26 @@ package com.example.proyectotitulo;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
 public class Valorar extends AppCompatActivity {
+    private Button mEnviar;
+    private TextView mTitulo;
+    private ImageView mImage;
+    private RatingBar mRBestado, mRBtrato, mRBpuntualidad;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +36,27 @@ public class Valorar extends AppCompatActivity {
             getSupportActionBar().setTitle("Valorar");
         }
 
+        mEnviar = (Button) findViewById(R.id.BtnEnviarValorar);
+        mTitulo = (TextView) findViewById(R.id.TVtituloPublicacionValorar);
+        mImage = (ImageView) findViewById(R.id.imageViewValorar);
+        mRBestado = (RatingBar) findViewById(R.id.ratingBar);
+        mRBtrato = (RatingBar) findViewById(R.id.ratingBar2);
+        mRBpuntualidad = (RatingBar) findViewById(R.id.ratingBar3);
 
+
+
+        mEnviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               SaveValoracion();
+            }
+        });
+    }
+
+    private void SaveValoracion(){
+        Log.d("rating", "estado producto " + mRBestado.getRating());
+        Log.d("rating", "estado producto " + mRBestado.getRating());
+        Log.d("rating", "estado producto " + mRBestado.getRating());
     }
 
     //toolbar
