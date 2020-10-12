0e144124a74592da47c3f86997418e52530decb2
Lucas Alarcon <lucas.alarcon.g@mail.pucv.cl>
2020-10-12 16:31:31 -0300

    activity_valorar.xml

---
+++ app/src/main/java/com/example/proyectotitulo/Valorar.java
@@ -0,0 +1,35 @@
+package com.example.proyectotitulo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class Valorar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_valorar);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle("Detalle publicacion");
+        }
+
+
+    }
+
+    //toolbar
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        Intent myIntent = new Intent(getApplicationContext(), PaginaPrincipal.class);
+        startActivityForResult(myIntent, 0);
+        return true;
+    }
+}
