744ab402fff9c376d52531a9fc665447caffbe2a
Claudio Jiménez <nectdnb@gmail.com>
2020-10-30 11:32:13 -0300

    merge arreglado

--- app/src/main/java/com/example/proyectotitulo/ChatViewHolders.java
+++ app/src/main/java/com/example/proyectotitulo/ChatViewHolders.java
@@ -8,12 +8,11 @@ import androidx.recyclerview.widget.RecyclerView;
 
 public class ChatViewHolders extends RecyclerView.ViewHolder implements View.OnClickListener{
     public TextView mMessage;
-    public TextView mUser;
+    public TextView mNombre,mHoraMensaje;
     public LinearLayout mContainer;
     public ChatViewHolders(View itemView) {
         super(itemView);
         itemView.setOnClickListener(this);
-        mUser = itemView.findViewById(R.id.nombreUserMensaje);
         mMessage = itemView.findViewById(R.id.message);
         mContainer = itemView.findViewById(R.id.container);
         mNombre = itemView.findViewById(R.id.nombreUserMensaje);
