926cb1e24eb98b34863d879671419877b185ee18
Claudio Jiménez <nectdnb@gmail.com>
2020-11-05 15:09:58 -0300

    cambiar contraseña con boton para volver atras

--- app/src/main/java/com/example/proyectotitulo/resetPassword.java
+++ app/src/main/java/com/example/proyectotitulo/resetPassword.java
@@ -1,6 +1,8 @@
 package com.example.proyectotitulo;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -8,6 +10,7 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -22,7 +25,13 @@ public class resetPassword extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reset_password);
-
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle("Cambiar Contraseña");
+        }
         mEmail = (EditText) findViewById(R.id.emailChange);
         enviarCorreo = (Button) findViewById(R.id.changeBtn);
         fAuth = FirebaseAuth.getInstance();
@@ -51,6 +60,14 @@ public class resetPassword extends AppCompatActivity {
             }
         });
 
+    }
 
+    //toolbar
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        Intent myIntent = new Intent(getApplicationContext(), Login.class);
+        startActivity(myIntent);
+        finish();
+        return true;
     }
 }
